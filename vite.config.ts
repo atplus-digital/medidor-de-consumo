7faadbdabfed53a2ac6ac4e3ddf56ed021f0124a
unknown <gustavo.carbonera@atplus.com.br>
2026-02-25 14:19:24 -0300

    First Commit

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import tailwindcss from "@tailwindcss/vite";
+import { devtools } from "@tanstack/devtools-vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+const config = defineConfig({
+	plugins: [
+		devtools(),
+		tsconfigPaths({ projects: ["./tsconfig.json"] }),
+		tailwindcss(),
+		tanstackStart(),
+		viteReact(),
+		nitro(),
+	],
+	nitro: {
+		serverDir: "./",
+	},
+});
+
+export default config;
